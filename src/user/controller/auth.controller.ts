4eaddf7bbb5cd359b2de30c3b9c9529adc8f5c0c
necm1 <necmi.m.hasan@outlook.de>
2021-06-26 06:16:48 +0200

    added rate limiter to login

--- src/user/controller/auth.controller.ts
+++ src/user/controller/auth.controller.ts
@@ -1,6 +1,7 @@
 import {Controller, Post, Request, UseGuards} from '@nestjs/common';
 import {AuthGuard} from '@nestjs/passport';
 import {AuthService} from '../service/auth.service';
+import {Throttle, ThrottlerGuard} from "@nestjs/throttler";
 
 @Controller('auth')
 /**
@@ -11,7 +12,8 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {
   }
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(AuthGuard('local'), ThrottlerGuard)
+  @Throttle(5, 30)
   @Post()
   public async login(@Request() req) {
     return this.authService.login(req.user);
